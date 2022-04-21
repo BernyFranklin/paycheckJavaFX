5d9c23647cbd17690b9296e3f56da290a384a6d9
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-04-21 15:10:14 -0700

    Initial Commit, did all work around BS to set up a JAVA FX file.

---
+++ src/paycheckfx/paycheckFX.java
@@ -0,0 +1,55 @@
+/*
+ * PaycheckFX Version#1.0
+ * Frank Bernal
+ * CIS 084 Java Programming
+ * Input
+ * Output
+ * 21 April 2022
+ */
+
+package paycheckfx;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author frankbernal
+ */
+public class paycheckFX extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}

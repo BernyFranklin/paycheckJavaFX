2c80901a9e04a92d112bdf0789f27e059b341c27
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-04-22 11:00:41 -0700

    App works. Added comments for better navigation for other devs.

--- src/paycheckfx/paycheckFX.java
+++ src/paycheckfx/paycheckFX.java
@@ -22,8 +22,7 @@ import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 // Imports for added features
-import javafx.application.Platform;
-import javafx.scene.layout.HBox;
+import javafx.application.Platform;   // Using PLatform for exit button
 
 /*
  * This program takes user input for hours worked and pay rate and 
@@ -31,6 +30,7 @@ import javafx.scene.layout.HBox;
  * 
  */
 
+// Start paycheckFX
 public class paycheckFX extends Application {
     
     // Define constants
@@ -42,12 +42,13 @@ public class paycheckFX extends Application {
     private static Label     lblHours;
     private static Label     lblPayRate;
     private static Button    btnCompute;
-    private static Button    btnClear;
-    private static Button    btnExit;
+    private static Button    btnClear;      // Added button
+    private static Button    btnExit;       // Added button
     private static TextField txtHours;
     private static TextField txtPayRate;
     private static TextArea  txtPaycheck;
     
+    // Start start stage
     @Override
     public void start(Stage stage) {
         Pane root = new Pane();
@@ -56,7 +57,7 @@ public class paycheckFX extends Application {
         Scene scene = new Scene(root, 450, 320);
         stage.setTitle("JavaFX Paycheck Project");
         stage.setResizable(false);
-        stage.setScene(scene);
+        stage.setScene(scene);           // Don't forget to "Set the Scene"
         stage.show();
         
     }   // End of start stage
@@ -69,10 +70,10 @@ public class paycheckFX extends Application {
         
     }   // End of PSV main
     
-    /*
-     * Process the Compute button click event
-     */
+    // Start btnComputeClick
+    // This method calculates the check when compute is clicked
     private static void btnComputeClick() {
+        // Variables for calculations
         double hours;
         double payRate;
         double regHours;
@@ -124,25 +125,36 @@ public class paycheckFX extends Application {
         
     }   // End of btnComputeClick
     
-    /*
-     *   Create the controls and place them on the pane
-     */
+    
+    // Start btnClearClick
+    // This method clears all the text areas when the clear button is clicked
+    private static void btnClearClick() {
+        txtHours.setText("");
+        txtPayRate.setText("");
+        txtPaycheck.setText("");
+    }   // End of btnClearClick
+    
+    // Start createControls
+    // Creates the controls and places them on the pane
     private static void createControls(Pane root) {
         Font font36B = Font.font("Ariel", FontWeight.BOLD, 36);   // Title
-        Font font18  = Font.font("Ariel", FontWeight.NORMAL, 18);
+        Font font18  = Font.font("Ariel", FontWeight.NORMAL, 18); // Regular
         
+        // Displayed at top of window
         lblTitle = new Label ("Frank's Paycheck Calculator");
             lblTitle.setFont(font36B);   // Big fat font
             lblTitle.setLayoutX(12);
             lblTitle.setLayoutY(4);
             root.getChildren().add(lblTitle);
             
+        // Displayed on left of text block    
         lblHours = new Label ("Hours");
             lblHours.setFont(font18);
             lblHours.setLayoutX(24);
             lblHours.setLayoutY(57);
             root.getChildren().add(lblHours);
-            
+        
+        // Displayed on right of label
         txtHours = new TextField();
             txtHours.setFont(font18);
             txtHours.setLayoutX(123);
@@ -151,12 +163,14 @@ public class paycheckFX extends Application {
             txtHours.setMinSize(119, 30);
             root.getChildren().add(txtHours);
             
+        // Displayed on left of text block    
         lblPayRate = new Label ("Pay Rate");
             lblPayRate.setFont(font18);
             lblPayRate.setLayoutX(24);
             lblPayRate.setLayoutY(101);
             root.getChildren().add(lblPayRate);
             
+        // Displayed on right of label    
         txtPayRate = new TextField();
             txtPayRate.setFont(font18);
             txtPayRate.setLayoutX(123);
@@ -165,6 +179,7 @@ public class paycheckFX extends Application {
             txtPayRate.setMinSize(119, 30);
             root.getChildren().add(txtPayRate);
             
+        // Button Displayed below above Labels and TextFields
         btnCompute = new Button ("Compute");
             btnCompute.setFont(font18);
             btnCompute.setLayoutX(40);
@@ -176,27 +191,34 @@ public class paycheckFX extends Application {
             btnCompute.setOnKeyPressed( e -> btnComputeClick());
             root.getChildren().add(btnCompute);
             
+        // Button Displayed to the right of Compute Button    
         btnClear = new Button ("Clear");
             btnClear.setFont(font18);
             btnClear.setLayoutX(170);
             btnClear.setLayoutY(148);
             btnClear.setMaxSize(110, 40);
             btnClear.setMinSize(110, 40);
-            // Action code here
+            // Event is found in btnClearClick
+            btnClear.setOnAction( e -> btnClearClick());
+            btnClear.setOnKeyPressed( e -> btnClearClick());
             root.getChildren().add(btnClear);
             
+        // Button Displayed to the right of Clear Button    
         btnExit = new Button ("Exit");
             btnExit.setFont(font18);
             btnExit.setLayoutX(300);
             btnExit.setLayoutY(148);
             btnExit.setMaxSize(110, 40);
             btnExit.setMinSize(110, 40);
-            //Action code here
+            // Event exits the application
+            btnExit.setOnAction( e -> Platform.exit());
+            btnExit.setOnKeyPressed( e -> Platform.exit());
             root.getChildren().add(btnExit);
             
+        // Displayed at bottom of window
         txtPaycheck = new TextArea();
             txtPaycheck.setFont(font18);
-            txtPaycheck.setLayoutX(40);
+            txtPaycheck.setLayoutX(25);
             txtPaycheck.setLayoutY(205);
             txtPaycheck.setMaxSize(400, 100);
             txtPaycheck.setMinSize(400, 100);
